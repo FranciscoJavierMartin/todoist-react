00c8650ca478ef1c381d7faf6d6c8d431bb051f2
Francisco <javiermartincuenca@gmail.com>
2021-05-02 21:06:46 +0200

    Sidebar component markup

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -1,11 +1,44 @@
-import React from 'react'
+import React from 'react';
+import {
+  FaChevronDown,
+  FaInbox,
+  FaRegCalendarAlt,
+  FaRegCalendar,
+} from 'react-icons/fa';
 
-const Sidebar:React.FC = () => {
+const Sidebar: React.FC = () => {
   return (
-    <div>
-      
+    <div className='sidebar' data-testid='sidebar'>
+      <ul className='sidebar_generic'>
+        <li>
+          <span>
+            <FaInbox />
+          </span>
+          <span>Inbox</span>
+        </li>
+        <li>
+          <span>
+            <FaRegCalendar />
+          </span>
+          <span>Today</span>
+        </li>
+        <li>
+          <span>
+            <FaRegCalendarAlt />
+          </span>
+          <span>Next 7 days</span>
+        </li>
+      </ul>
+      <div className='sidebar__middle'>
+        <span>
+          <FaChevronDown />
+        </span>
+        <h2>Projects</h2>
+      </div>
+      <ul className='sidebar__projects'>Projects will be here</ul>
+      Add project
     </div>
-  )
-}
+  );
+};
 
-export default Sidebar
+export default Sidebar;
