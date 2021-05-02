95a259e63bdac28b95c45d00541c33c6b20a547b
Francisco <javiermartincuenca@gmail.com>
2021-05-02 20:58:02 +0200

    Style header

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import Content from './components/layout/Content';
+import Header from './components/layout/Header';
 
-function App() {
-  return <div></div>;
-}
+const App: React.FC = () => {
+  return (
+    <div>
+      <Header />
+      <Content />
+    </div>
+  );
+};
 
 export default App;
